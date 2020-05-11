665e0f1889086a7d94319d16d86c10851c7ba920
alebogi <no.oneplayer@gmail.com>
2020-05-11 14:19:31 +0200

    demo v1

---
+++ faza 6 - Implementacija veb aplikacije/hoceneko_demo/app/Views/stranice/Info.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Ognjen Subaric 0425/17
+ * @coauthor Aleksandra Bogicevic 0390/17
+ */
+?>
+<div class="col-sm-7 col-md-7 col-lg-7 main">
+    <p>Ova veb aplikacija je nemanjena svima koji su željni sklapanja novih prijateljstava kroz organizovanje i dogovaranje događaja različitog tipa. 
+        Nasa misija je da se zamene raznorazne grupe sa društvenih mreža jedinstvenom platformom koja će omogućiti brzo pronalaženje željenih aktivnosti koje će 
+        biti raspoređene po sekcijama, na jedan klik od Vas. 
+    </p>
+    <p>
+        Svima se dešavalo da nekada otkažu planove jer nisu imali sa kime da ih realizuju. 
+        Ovaj sajt postoji upravo iz tih razloga. Nasa misija je da korisnicima omogucimo objave informacije o aktivnosti koju žele da realizuju, 
+        a svi zainteresovani će imati mogućnost da se priključe događaju.
+    </p>
+    <p>
+        Tim Nebula
+    </p>
+        
+</div>
---
+++ faza 6 - Implementacija veb aplikacije/hoceneko_demo/app/Views/stranice/login.php
@@ -0,0 +1,34 @@
+<?php
+    /**
+     * @author Aleksandra Bogicevic 0390/17
+     * @coauthor Nikola Milicevic 0387/17
+     */
+?>
+
+<div class="col-sm-7 col-md-7 col-lg-7 main">
+       <form name="loginform" action="<?= site_url("Gost/loginSubmit") ?>" method="post">
+<table>
+    <tr>
+        <?php if(isset($poruka)) echo "<font color='red'>$poruka</font><br><br>"; ?>
+        <td>Korisnicko ime:</td>
+        <td><input type="text" name="korisnicko_ime" 
+                   value="<?= set_value('korisnicko_ime') ?>"/></td>
+        <td><font color='red'>
+            <?php if(!empty($errors['korisnicko_ime'])) 
+                echo $errors['korisnicko_ime'];
+            ?></font></td>
+    </tr>
+    <tr>
+        <td>Lozinka:</td>
+        <td><input type="password" name="lozinka"/></td>
+        <td><font color='red'>
+             <?php if(!empty($errors['lozinka'])) 
+                echo $errors['lozinka'];
+            ?></font></td>
+    </tr>
+    <tr>
+        <td><input type="submit" value="Log in"/></td>
+    </tr>
+</table>
+           </form>
+ </div>
