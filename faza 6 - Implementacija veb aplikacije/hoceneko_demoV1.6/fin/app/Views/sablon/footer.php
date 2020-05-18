0208d4ae18bc79419bf2b69ec87be8e97b28d4f6
milka1g <nikola260896@gmail.com>
2020-05-18 21:13:02 +0200

    testirajte

---
+++ faza 6 - Implementacija veb aplikacije/hoceneko_demoV1.6/fin/app/Views/sablon/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Aleksandra Bogicevic 0390/17
+ */
+?>
+
+<!-- pocetak footera -->
+ <div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 bottom ftr">
+        © Nebula 2020. All rights reserved.
+    </div>
+</div>
+</div>
+</body>
+</html>
---
+++ faza 6 - Implementacija veb aplikacije/hoceneko_demoV1.6/fin/app/Views/stranice/primerMain.php
@@ -0,0 +1,6 @@
+ <!-- main deo gde se ubacuju stranice -->
+ <div class="col-sm-7 col-md-7 col-lg-7 main">
+                Ovde je glavni deo
+ </div>
+<!-- kraj main dela koji ce se ubacivati - "stranice" -->
+            
