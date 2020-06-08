9ac5802c13853f4bf2ccf276d4427a9084a5d641
Coakim <acamatc98x12@gmail.com>
2020-06-08 15:11:47 +0200

    hoceneko_finalv1.1
    Zavrsna verzija projekta sa ispravljenim registrovanjem korisnika (bez izbore slike)

---
+++ faza 6 - Implementacija veb aplikacije/hoceneko_finalv1.1/app/Models/ObjavaModel.php
@@ -0,0 +1,176 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+/*
+ * Klasa ObjavaModel - klasa modela koji sluzi za dohvatanje podataka iz tabele Objava u bazi podataka
+ * 
+ * 
+ */
+class ObjavaModel extends Model
+{
+        protected $table      = 'objava';
+        protected $primaryKey = 'idObj';
+        protected $returnType = 'object';
+        protected $allowedFields = ['naziv', 'kategorija', 'br_potrebnih_clanova', 'br_prijavljenih_clanova', 'datum', 'vreme' , 'mesto', 'opis' , 'tip' , 'idKor'];
+        
+        /*
+         *  Funkcija dohvatiPoFilterima - sluzi za dohvatanje podataka iz tabele Objava na osnovu prosledjenih filtera
+         * @param Date $datum_od
+         * @param Date $datum_do
+         * @param String $mesto
+         * @param Time $vreme_od
+         * @param Time $vreme_do
+         * 
+         * @var QueryBuilder $upit
+         * 
+         * return String[] 
+         * 
+         * @autor Aleksandar Matic
+         * 
+         */
+        public function dohvatiPoFilterima($datum_od,$datum_do,$mesto,$vreme_od,$vreme_do){
+            $upit;
+            if(!empty($datum_od)){
+               $upit = $this->where('datum >=',$datum_od);
+            }
+            if(!empty($datum_do)){
+                if(!empty($upit)){
+                $upit = $upit->where('datum <=',$datum_do);
+                }
+                else $upit = $this->where('datum <', $datum_do);
+            }
+            if(!empty($mesto)){
+                if(!empty($upit)){
+                $upit = $upit->like('mesto',$mesto);
+                }
+                else $upit = $this->like('mesto',$mesto);
+            }
+            if(!empty($vreme_od)){
+               if(!empty($upit)){
+                $upit = $upit->where('vreme >=',$vreme_od);
+                }
+                else $upit = $this->where('vreme >=', $vreme_od);
+            }
+            if(!empty($vreme_do)){
+                if(!empty($upit)){
+                $upit = $upit->where('vreme <=',$vreme_do);
+                }
+                else $upit = $this->where('vreme <=', $vreme_do);
+            }
+            if(!empty($upit)){
+                return $upit->paginate(10);
+            }
+            else return $this->paginate(10);
+        }
+        
+        /**
+     * Funckija koja pretrazuje bazu da bi nasla objave koje odgovaraju kriterijumu
+     * 
+     * @param string $kategorija kategorija po kojoj se vrsi pretraga
+     * @return resoult
+     * 
+     * @author Ognjen Subaric 0425/17
+     */
+    public function pretraga($kategorija) {
+        if($kategorija == 'sve'){
+            return $this->where('datum >=', date("Y-m-d"))->orderBy('tip', 'DESC')->orderBy('datum', 'ASC')->findAll();
+        }
+        return $this->like('kategorija', $kategorija)->where('datum >=', date("Y-m-d"))->orderBy('tip', 'DESC')->orderBy('datum', 'ASC')->findAll();      
+    }
+    
+    /**
+     * Pomocna funckija koja pomaze pri upitu koji podrazumeva spajanje dve tabele
+     * 
+     * @param string $order kriterijum sortiranja
+     * @param string $kategorija kategorija po kojoj se vrsi pretraga
+     * @return resoult 
+     * 
+     * @author Ognjen Subaric 0425/17
+     */
+    private function sortOceneKorisnika($order, $kategorija){
+        $db      = \Config\Database::connect();
+        $builder = $db->table('objava');
+        $builder->select('*');
+        $builder->join('korisnik', 'korisnik.idKor = objava.idKor');
+        if($kategorija != "sve"){
+            $builder->like("kategorija", $kategorija);
+        }
+        $builder->where('datum >=', date("Y-m-d"));
+        $builder->orderBy("objava.tip", "DESC");
+        $builder->orderBy("ocena", $order);
+        $query = $builder->get();
+        $result = $query->getResult();
+//        forEach($result as $res){
+//            echo $res->naziv . "<br>";
+//            echo $res->kategorija . "<br>";
+//            echo $res->korisnicko_ime . "<br>";
+//            echo $res->ime . "<br>";
+//            echo $res->ocena . "<br>";
+//            echo "------------------<br>";
+//        }
+        return $result;
+    }
+    
+    /**
+     * Funckija koja pretrazuje bazu da bi nasla objave koje odgovaraju kriterijumima
+     * 
+     * @param string $kategorija
+     * @param string $order kriterijum sortiranja
+     * @return resoult
+     * 
+     * @author Ognjen Subaric 0425/17
+     */
+    public function pretragaSort($kriterijum, $order, $kategorija){
+        if($kriterijum == "ocena")
+            return $this->sortOceneKorisnika($order, $kategorija);
+        if($kategorija == 'sve')
+            return $this->where('datum >=', date("Y-m-d"))->orderBy('tip', 'DESC')->orderBy($kriterijum, $order)->findAll();
+        return $this->like('kategorija', $kategorija)->where('datum >=', date("Y-m-d"))->orderBy('tip', 'DESC')->orderBy($kriterijum, $order)->findAll();
+    }
+    
+    /**
+     * Metoda za dohvatanje broja potrebnih ljudi
+     * 
+     * @param int $idObj primarni kljuc tabele objava
+     * @return int broja potrebnih ljudi
+     * 
+     * @author Ognjen Subaric 0425/17
+     */
+    public function dohvatiBrPotrebnihClanova($idObj){
+        return $this->find($idObj)->br_potrebnih_clanova;
+    }
+    
+    /**
+     * Metoda za dohvatanje broja prijavljenih ljudi
+     * 
+     * @param int $idObj primarni kljuc tabele objava
+     * @return int broja prijavljenih ljudi
+     * 
+     * @author Ognjen Subaric 0425/17
+     */
+    public function dohvatiBrPrijavljenihClanova($idObj){
+        return $this->find($idObj)->br_prijavljenih_clanova;
+    }
+    
+    /**
+     * Metoda koja povecava broj prijavljenih ljudi
+     * 
+     * @param int $idObj primarni kljuc tabele objava
+     * 
+     * @author Ognjen Subaric 0425/17
+     */
+    public function povecajBrPrijavljenih($idObj){
+        $stariBrojPrijavljenih = $this->find($idObj)->br_prijavljenih_clanova;
+        $data = [
+        'br_prijavljenih_clanova'    => $stariBrojPrijavljenih + 1
+        ];
+        $this->update($idObj, $data);
+    }
+    public function smanjiBrPrijavljenih($idObj){
+        $stariBrojPrijavljenih = $this->find($idObj)->br_prijavljenih_clanova;
+        $data = [
+        'br_prijavljenih_clanova'    => $stariBrojPrijavljenih - 1
+        ];
+        $this->update($idObj, $data);
+    }
+}
---
+++ faza 6 - Implementacija veb aplikacije/hoceneko_finalv1.1/app/Views/stranice/register.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * 
+ * @author Nikola Milicevic 0387/17
+ * @coauthor Aleksandra Bogicevic 0390/17
+ */
+?>
+
+<!-- main deo gde se ubacuju stranice -->
+ <div class="col-sm-8 col-md-8 col-lg-8 main ">
+      
+        
+     <div class="row registracija justify-content-center">
+          
+         <div class="col-sm-3 col-md-3 col-lg-3">
+             <!-- namerno prazno -->
+         </div>
+         <div class="col-sm-3 col-md-3 col-lg-3">
+             <?php
+                echo form_open_multipart("Gost/registerSubmit","method=post");
+
+                echo form_label('Korisnicko ime', 'korisnicko_ime');
+             echo("<br>");
+              echo form_label('Lozinka', 'lozinka');
+              echo("<br>");
+              echo form_label('Potvrdite lozinku', 'lozinka_p');
+               echo("<br>");
+               echo form_label('Ime', 'ime');
+
+                   echo("<br>");
+                   echo form_label('Prezime', 'prezime');
+
+                   echo("<br>");
+                   echo form_label('E-mail', 'e_mail');
+
+                   echo("<br>");
+                   echo form_label('Pol', 'pol');
+                   echo("<br>");
+                   echo form_label('Datum rodjenja', 'datum_rodjenja');
+
+                   echo "<br>Opis:<br/>";
+                   echo("<br>");
+                    echo("<br>");
+                     echo("<br>");
+                      echo("<br>");
+                       echo("<br>");
+                       echo("<br>");
+                      echo("<br>");
+                       echo("<br>");
+                       echo("<br>");
+                    
+                   echo "Profilna slika:<br>";
+
+                   echo("<br>");
+
+               
+             ?>
+         </div>
+         <div class="col-sm-3 col-md-3 col-lg-3">
+             <?php
+              echo form_input(['name' => 'korisnicko_ime']);
+             echo("<br>");
+             echo form_password(['name' => 'lozinka']);
+                   echo("<br>");
+                   echo form_password(['name' => 'lozinka_p']);
+                    echo("<br>");
+                    
+                   echo form_input(['name' => 'ime']);
+
+                   echo("<br>");
+                   echo form_input(['name' => 'prezime']);
+
+                   echo("<br>");
+                   echo form_input(['name' => 'e_mail']);
+
+                   echo("<br>");
+                   $options = [
+                           'muski'  => 'muski',
+                           'zenski'    => 'zenski',
+                   ];
+
+                   echo form_dropdown('pol',$options,'muski');
+                   echo("<br>");
+                   echo("<input type='date' name='datum_rodjenja'>");
+
+                   echo form_textarea("opis",set_value("opis"), ['maxlength'=> 250]);
+                   echo("<br>");
+                   echo "<input type='file' name='profilna_slika' />";
+                   echo("<br>");
+                   
+                   //dodatak
+                   
+                   //kraj dodatka
+
+                   //odavde uzeto
+                  /* $str = "pocetna";
+                   echo("<input class='btn btn-sm btn-info' onmousedown='storeActiveNav($str)' type='submit' value='Registruj se'/>");
+                   //echo form_submit('registruj_se', 'Registruj se');
+                   echo form_close();*/
+             ?>
+         </div>
+         <div class="col-sm-3 col-md-3 col-lg-3">
+             <!-- namerno prazno -->
+         </div>
+         <?php 
+         //ovde dodato
+         echo("Da li se slazete sa "); ?>
+         
+         <a href="#" onclick="usloviKoriscenja()">&nbsp; uslovima koriscenja &nbsp;</a>
+         
+        <?php
+        echo("aplikacije?");
+         echo("<br>");
+         echo("<br>");
+            $opcije = [
+                  'Da'  => 'Da',
+                   'Ne'    => 'Ne',
+            ];
+
+          echo form_dropdown('saglasnost',$opcije,'Da');
+          echo("<br><br>");
+          
+          
+          
+           $str = "pocetna";
+            echo("<input class='btn btn-sm btn-info' onmousedown='storeActiveNav($str)' type='submit' value='Registruj se'/>");
+             echo form_close();
+         
+         ?>
+         
+         
+             <?php if(isset($poruka)) echo "<font color='red'>$poruka</font><br><br>";
+        if(isset($errors)) echo "<font color='red'>$errors</font><br><br>"; ?>
+     </div>
+     
+ </div>
